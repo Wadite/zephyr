3cb672f868b531bd8f8911d1c69bc033dc85d41d
Roee K <83594350+Wadite@users.noreply.github.com>
2023-03-05 13:55:57 +0200

    Update http_get.c

--- samples/net/sockets/http_get/src/http_get.c
+++ samples/net/sockets/http_get/src/http_get.c
@@ -27,6 +27,9 @@
 
 #endif
 
+#include <zephyr/net/net_mgmt.h>
+#include <zephyr/drivers/modem/hl7800.h>
+
 /* HTTP server to connect to */
 #define HTTP_HOST "google.com"
 /* Port to connect to, as string */
@@ -42,9 +45,66 @@
 #define SSTRLEN(s) (sizeof(s) - 1)
 #define CHECK(r) { if (r == -1) { printf("Error: " #r "\n"); exit(1); } }
 
-#define REQUEST "GET " HTTP_PATH " HTTP/1.0\r\nHost: " HTTP_HOST "\r\n\r\n"
+#define REQUEST "GET " HTTP_PATH " HTTP/1.0\r\nHost: www." HTTP_HOST "\r\n\r\n"
 
 static char response[1024];
+
+static bool is_network_ready = false;
+
+static void iface_dns_added_evt_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event,
+                    struct net_if *iface)
+{
+    if (mgmt_event != NET_EVENT_DNS_SERVER_ADD)
+    {
+        return;
+    }
+    printk("DNS ready\n");
+    is_network_ready = true;
+}
+
+static void iface_up_evt_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event,
+                    struct net_if *iface)
+{
+    if (mgmt_event != NET_EVENT_IF_UP)
+    {
+        return;
+    }
+    printk("interface is up\n");
+}
+
+static void iface_down_evt_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event,
+                    struct net_if *iface)
+{
+    if (mgmt_event != NET_EVENT_IF_DOWN)
+    {
+        return;
+    }
+    printk("Interface is down\n");
+    is_network_ready = false;
+}
+
+static struct mgmt_events {
+    uint32_t event;
+    net_mgmt_event_handler_t handler;
+    struct net_mgmt_event_callback cb;
+} iface_events[] = {
+    {.event = NET_EVENT_DNS_SERVER_ADD, .handler = iface_dns_added_evt_handler},
+    {.event = NET_EVENT_IF_UP,          .handler = iface_up_evt_handler},
+    {.event = NET_EVENT_IF_DOWN,        .handler = iface_down_evt_handler},
+    {0} /* setup_iface_events requires this extra location. */
+};
+
+static void setup_iface_events(void)
+{
+    int i;
+    for (i = 0; iface_events[i].event; i++)
+    {
+        net_mgmt_init_event_callback(&iface_events[i].cb, iface_events[i].handler,
+                            iface_events[i].event);
+
+        net_mgmt_add_event_callback(&iface_events[i].cb);
+    }
+}
 
 void dump_addrinfo(const struct addrinfo *ai)
 {
@@ -61,6 +121,21 @@ void main(void)
 	struct addrinfo *res;
 	int st, sock;
 
+	setup_iface_events();
+
+    	st = mdm_hl7800_reset();
+    	if (st != 0)
+    	{
+        	printk("modem init err: %d\n", st);
+	        return;
+    	}
+
+	    while (!is_network_ready)
+    	{
+        	printk("Waiting for network to be ready...\n");
+        	k_sleep(K_SECONDS(5));
+    	}
+	
 #if defined(CONFIG_NET_SOCKETS_SOCKOPT_TLS)
 	tls_credential_add(CA_CERTIFICATE_TAG, TLS_CREDENTIAL_CA_CERTIFICATE,
 			   ca_certificate, sizeof(ca_certificate));
